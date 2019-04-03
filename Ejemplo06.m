e4b0803babfa7d3c57f809ea1ea9366395e2abe3
NightRoadIx <quintanarxbio@gmail.com>
2019-04-03 09:59:07 -0600

    Add files via upload

--- Ejemplo06.m
+++ Ejemplo06.m
@@ -1,5 +1,6 @@
 % Practica 6
 % Ensanchamiento/Disminución Contraste
+% Esto también se le puede llamar expansión del histograma
 clear all
 close all
 clc
@@ -28,8 +29,39 @@ while(1)
     % como si fuera el negativo de la imagen en ese intervalo
     warning('<Cambio en contraste no posible, teclear valores de nuevo>')
 end
-%Se aplica el algoritmo
-imgrayac=rectas(imgray,r1,s1,r2,s2);
+
+% Se aplica el algoritmo
+% imgrayac=rectas(imgray,r1,s1,r2,s2);
+[sx,sy,sz]=size(imgray);
+%obtener las pendientes
+m1=s1/r1;
+m2=(s2-s1)/(r2-r1);
+m3=(255-s2)/(255-r2);
+%se deben de obtener las ecuaciones de las rectas
+% y1=m1*x1
+% y2=m2*(x2-r2)+s2
+% y3=m3*(x3-255)+255
+%Se aplican sobre la imagen
+for k=1:sz
+    for i=1:sx
+        for j=1:sy
+            val=imgray(i,j,k);
+            %primera recta
+            if (val >= 0)&&(val < r1)
+                imgrayac(i,j,k)=m1*val;
+            end
+            %segunda recta
+            if (val >= r1)&&(val < r2)
+                imgrayac(i,j,k)=m2*(val-r1)+s1;
+            end
+            %tercera recta
+            if (val >= r2)&&(val <= 255)
+                imgrayac(i,j,k)=m3*(val-r2)+s2;
+            end
+        end
+    end
+end
+
 %Y las graficas de las imagenes
 subplot(2,2,1), imshow(imgray); title('Imagen original')
 subplot(2,2,2), imshow(uint8(imgrayac)); title('Imagen modificada en contraste')
