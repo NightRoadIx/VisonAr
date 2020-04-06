5e99689e10b3fefd945db99d6691541795c77236
NightRoadIx <quintanarxbio@gmail.com>
2020-04-06 01:12:47 -0500

    Add files via upload

--- exempel028.py
+++ exempel028.py
@@ -7,25 +7,30 @@ cap = cv2.VideoCapture(0)
 while(1):
 
     # Captura cada cuadro
-    _, frame = cap.read()
+    ret, frame = cap.read()
 
-    # Convertir de espacio de color BGR a HSV
+    # Convertir de espacio de color BGR a HSV (esto permite identificar mejor la piel humana)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # Definir el intervalo del color azul en HSV
+    # Definir el intervalo del color rojo en HSV
     lower_red = np.array([0,50,50])
     upper_red = np.array([30,255,255])
 
+    # Definir el intervalo del color verde en HSV
     lower_green = np.array([60,50,50])
     upper_green = np.array([90,255,255])
 
+    # Definir el intervalo del color azul en HSV
     lower_blue = np.array([130,50,50])
     upper_blue = np.array([150,255,255])
 
+    # Definir el intervalo del color violeta en HSV
     lower_violet = np.array([150,50,50])
     upper_violet = np.array([200,255,255])
+	# RECORDAR QUE HSV REPRESENTA EL MATIZ, LA SATURACIÓN Y EL VALOR POR EL CONO DE COLOR
 
     # Umbralizar la imagen HSV para que solo se obtengan los colores azules
+	# Esto genera una especie de "máscara" con los valores de los colores
     mask = cv2.inRange(hsv, lower_red, upper_red)
     mask2 = cv2.inRange(hsv, lower_green, upper_green)
     mask3 = cv2.inRange(hsv, lower_blue, upper_blue)
@@ -37,6 +42,7 @@ while(1):
     res3 = cv2.bitwise_and(frame,frame, mask= mask3)
     res4 = cv2.bitwise_and(frame,frame, mask= mask4)
 
+    # Observar el resultado de la aplicación de las máscaras
     cv2.imshow('frame',frame)
 	#cv2.imshow('hsv',hsv)
     #cv2.imshow('mask',mask)
