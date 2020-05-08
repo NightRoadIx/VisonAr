d03a870f0935933b3b48a32484b58151579f9b99
NightRoadIx <quintanarxbio@gmail.com>
2020-05-08 01:17:56 -0500

    Add files via upload

--- exempel029.py
+++ exempel029.py
@@ -1,9 +1,12 @@
-# Clasificación de imagenes
+#%%
+# Clasificación de imágenes
 from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+from keras.utils import plot_model
 
 import os
 import numpy as np
@@ -26,7 +29,7 @@ cats_and_dogs_filtered
     |______ dogs: [dog.2000.jpg, dog.2001.jpg, dog.2002.jpg ...]
 
 '''
-
+#%%
 # Una vez que se extraen los datos, se asignan las variables para los grupos de 
 # entrenamiento y validación
 train_dir = os.path.join(PATH, 'train')
@@ -39,6 +42,7 @@ train_dogs_dir = os.path.join(train_dir, 'dogs')  # imágenes de perros
 validation_cats_dir = os.path.join(validation_dir, 'cats')  # imágenes de gatos
 validation_dogs_dir = os.path.join(validation_dir, 'dogs')  # imágenes de perros
 
+#%%
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Analizar los datos que se obtuvieron
 
@@ -60,6 +64,7 @@ epochs = 50
 IMG_HEIGHT = 150
 IMG_WIDTH = 150
 
+#%%
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Preparación de los datos
 
@@ -80,7 +85,7 @@ val_data_gen = validation_image_generator.flow_from_directory(batch_size=batch_s
                                                               directory=validation_dir,
                                                               target_size=(IMG_HEIGHT, IMG_WIDTH),
                                                               class_mode='binary')
-
+#%%
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Visualizar las imágenes
 
@@ -92,7 +97,7 @@ sample_training_images, _ = next(train_data_gen)
 
 # Esta función graficará las imágenes en forma de una rejilla 1 x 5
 def plotImages(images_arr):
-    fig, axes = plt.subplots(1, 5, figsize=(20,20))
+    fig, axes = plt.subplots(2, 2, figsize=(20,20))
     axes = axes.flatten()
     for img, ax in zip( images_arr, axes):
         ax.imshow(img)
@@ -102,14 +107,27 @@ def plotImages(images_arr):
 
 plotImages(sample_training_images[:5])
 
+#%%
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Crear el modelo
 
 # Aquí se genera un modelo que consiste en tres bloques de convolución 
 # con las capas máximas pool en cada uno de ellos.
-# Hay una capa completamente conectada con 512 unidades que son activadas
+# Las capas de convolución (Conv2D) extraen características de la 
+# imagen. La convolución ayuda al emborronamiento, agudizado, detección de bordes
+# reducción de ruido y otras operaciones que ayudan a obtener las características
+# específicas de la imagen.
+# Max pooling es un proceso de discretización basado en muestras
+# su objetivo es disminuir el número de muestras como representación de entrada,
+# reduciendo así su dimensionalidad y permitiendo que se realicen suposiciones
+# acerca de las características contenidas en las sub-regiones.
+# La instrucción Flatten lo que realiza es convertir los datos de salida
+# en un vectro simple de n elementos (en este caso al estar a la salida de una capa
+# con 64 neuronas, se genera un vector de 64 elementos)
+# Hay una capa completamente conectada con 512 unidades que son activadas (Dense)
 # por una FA del tipo relu (Rectified Linear Unit, y = max(0, x))
 # La salida del modelo es una clasificación binaria mediante la FA sigmoide
+# (binaria pues se están clasificando dos clases de fotografías)
 model = Sequential([
     Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH ,3)),
     MaxPooling2D(),
@@ -128,10 +146,11 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 # Observar el resumen del modelo generado 
-model.summary()
+print(model.summary())
 
+#%%
 # Entrenar el modelo
-'''
+
 history = model.fit_generator(
     train_data_gen,
     steps_per_epoch=total_train // batch_size,
@@ -151,19 +170,24 @@ epochs_range = range(epochs)
 
 plt.figure(figsize=(8, 8))
 plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.plot(epochs_range, acc, label='Precisión de Entrenamiento')
+plt.plot(epochs_range, val_acc, label='Precisión de Validación')
 plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
+plt.title('Precisión de Entrenamiento y Validación')
 
 plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.plot(epochs_range, loss, label='Pérdida de Entrenamiento')
+plt.plot(epochs_range, val_loss, label='Pérdida de Validación')
 plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
+plt.title('Pérdida de Entrenamiento y Validación')
 plt.show()
-'''
+
+#%%
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# En las gráficas anteriores se observa que la precisión del entrenamiento crece
+# de forma lineal con el tiempo, mientras que la precisión de validación se estanca
+# en alrededor del 70% en el proceso de entrenamiento. Además la diferencia en precisión
+# entre el entrenamiento y la validación es evidente -un signo del overfitting
 # Eliminar el overfitting por medio del aumento de los datos de entrenamiento
 # lo que se hace es, en primer lugar, evitar que la red sea entrenada
 # o que vea la misma imagen dos veces durante el entrenamiento
@@ -184,6 +208,7 @@ augmented_images = [train_data_gen[0][0][0] for i in range(5)]
 # Visualizar las nuevas imágenes
 plotImages(augmented_images)
 
+#%%
 # Rotar la imagen de manera aleatoria
 image_gen = ImageDataGenerator(rescale=1./255, rotation_range=45)
 
@@ -196,7 +221,7 @@ augmented_images = [train_data_gen[0][0][0] for i in range(5)]
 
 plotImages(augmented_images)
 
-
+#%%
 # Ahora hacer zoom
 image_gen = ImageDataGenerator(rescale=1./255, zoom_range=0.5)
 
@@ -209,6 +234,7 @@ augmented_images = [train_data_gen[0][0][0] for i in range(5)]
 
 plotImages(augmented_images)
 
+#%%
 # Juntar todos estos grupos de datos
 image_gen_train = ImageDataGenerator(
                     rescale=1./255,
@@ -236,7 +262,7 @@ val_data_gen = image_gen_val.flow_from_directory(batch_size=batch_size,
                                                  directory=validation_dir,
                                                  target_size=(IMG_HEIGHT, IMG_WIDTH),
                                                  class_mode='binary')
-
+#%%
 # Otra técnica para reducir el overfitting es la introducción de dropouts a la red
 # Es una forma de regularización que fuerza a los pesos neuronales a tomar
 # solo valores pequeños, lo que hace la distribución de los pesos más regulares
